acf24948c7df51753807d6891786f523a62cc930
rafaelbiten <rafaelbiten@icloud.com>
2021-03-26 21:50:46 -0400

    [FEAT] Exercise - simple recursive triple fn

--- servy/lib/playground/recurse.ex
+++ servy/lib/playground/recurse.ex
@@ -3,9 +3,17 @@ defmodule Recurse do
 
   def sum([], total), do: total
   def sum([x | xs], total), do: sum(xs, total + x)
+
+  def triple([]), do: []
+  def triple([x | xs]), do: [x * 3 | triple(xs)]
 end
 
 1..5
 |> Enum.to_list()
 |> Recurse.sum()
+|> IO.puts()
+
+1..5
+|> Enum.to_list()
+|> Recurse.triple()
 |> IO.inspect()
