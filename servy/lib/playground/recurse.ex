8be762aee2c04da78456e963d56cb7ed3d7c15c1
rafaelbiten <rafaelbiten@icloud.com>
2021-03-26 22:01:12 -0400

    [REFACT] Recurse.triple with TCO (tail call optim)

--- servy/lib/playground/recurse.ex
+++ servy/lib/playground/recurse.ex
@@ -1,19 +1,19 @@
-defmodule Recurse do
+defmodule Playground.Recurse do
   def sum(list) when is_list(list), do: sum(list, 0)
-
-  def sum([], total), do: total
   def sum([x | xs], total), do: sum(xs, total + x)
+  def sum([], total), do: total
 
-  def triple([]), do: []
-  def triple([x | xs]), do: [x * 3 | triple(xs)]
+  def triple(list) when is_list(list), do: triple(list, [])
+  def triple([x | xs], tripled_list), do: triple(xs, [x * 3 | tripled_list])
+  def triple([], tripled_list), do: Enum.reverse(tripled_list)
 end
 
 1..5
 |> Enum.to_list()
-|> Recurse.sum()
+|> Playground.Recurse.sum()
 |> IO.puts()
 
 1..5
 |> Enum.to_list()
-|> Recurse.triple()
+|> Playground.Recurse.triple()
 |> IO.inspect()
