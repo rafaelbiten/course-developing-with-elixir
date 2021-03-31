17af49ea6ccbc6f142df2cd0e1531699962f7c42
rafaelbiten <rafaelbiten@icloud.com>
2021-03-30 21:54:44 -0400

    [FEAT] Implements map

--- servy/lib/playground/recurse.ex
+++ servy/lib/playground/recurse.ex
@@ -6,6 +6,11 @@ defmodule Playground.Recurse do
   def triple(list) when is_list(list), do: triple(list, [])
   def triple([x | xs], tripled_list), do: triple(xs, [x * 3 | tripled_list])
   def triple([], tripled_list), do: Enum.reverse(tripled_list)
+
+  def map([], _f), do: []
+  def map(list, f), do: map(list, f, [])
+  defp map([x | xs], f, result), do: map(xs, f, [f.(x) | result])
+  defp map([], _f, result), do: Enum.reverse(result)
 end
 
 1..5
@@ -32,3 +37,12 @@ triple = &(&1 * 3)
 # |> Enum.map(&(&1 * 3))
 |> Enum.map(triple)
 |> IO.inspect()
+
+1..6
+|> Enum.to_list()
+|> Playground.Recurse.map(triple)
+|> IO.inspect()
+
+Playground.Recurse.map(["hey", "ho"], &"I say #{&1}! ")
+|> Enum.join()
+|> IO.puts()
