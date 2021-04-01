8b0bde0c635a668441020a4ecc99fd2cba0d81f1
rafaelbiten <rafaelbiten@icloud.com>
2021-03-31 22:14:07 -0400

    [REFACT] Using eex template files

--- servy/lib/servy/bear_ctrl.ex
+++ servy/lib/servy/bear_ctrl.ex
@@ -2,24 +2,37 @@ defmodule Servy.BearCtrl do
   alias Servy.Conn
   alias Servy.Wildthings
 
+  @templates_path Path.expand("templates", File.cwd!())
+
   @spec index(Conn.t()) :: Conn.t()
   def index(%Conn{} = conn) do
     bears_by_name_asc = fn bear1, bear2 -> bear1.name <= bear2.name end
-    bear_to_list_item = fn bear -> "<li>#{bear.name} - #{bear.type}</li>" end
+    # bear_to_list_item = fn bear -> "<li>#{bear.name} - #{bear.type}</li>" end
 
-    bear_items =
+    bears =
       Wildthings.list_bears()
       |> Enum.sort(bears_by_name_asc)
-      |> Enum.map(bear_to_list_item)
-      |> Enum.join()
 
-    %{conn | status: 200, resp_body: "<ul>#{bear_items}</ul>"}
+    resp_body =
+      @templates_path
+      |> Path.join("index.eex")
+      |> EEx.eval_file(bears: bears)
+
+    %{conn | status: 200, resp_body: resp_body}
   end
 
   def show(%Conn{} = conn, %{"id" => id} = _params) do
     case Wildthings.get_bear(id) do
-      nil -> %{conn | status: 404, resp_body: "Bear with id '#{id}' not found!"}
-      bear -> %{conn | status: 200, resp_body: "#{bear.name} Bear"}
+      nil ->
+        %{conn | status: 404, resp_body: "Bear with id '#{id}' not found!"}
+
+      bear ->
+        resp_body =
+          @templates_path
+          |> Path.join("show.eex")
+          |> EEx.eval_file(bear: bear)
+
+        %{conn | status: 200, resp_body: resp_body}
     end
   end
 
