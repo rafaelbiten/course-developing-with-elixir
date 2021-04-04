c12e77583fff932e0add8e8f4b53bada326e159c
rafaelbiten <rafaelbiten@icloud.com>
2021-04-04 17:16:06 -0400

    [FEAT] Simple examples with comprehensions

---
+++ servy/lib/playground/comprehension.ex
@@ -0,0 +1,28 @@
+defmodule Playground.Comprehensions do
+end
+
+list = Enum.to_list(1..3)
+
+IO.puts("Normal map")
+Enum.map(list, &IO.puts/1)
+
+IO.puts("List comprehension")
+for x <- list, do: IO.puts(x)
+
+# ---------------------------------------
+
+style = %{"width" => 10, "height" => 20}
+
+IO.puts("Normal map")
+
+_ =
+  Map.new(style, fn {key, value} -> {String.to_atom(key), value} end)
+  |> IO.inspect()
+
+IO.puts("List comprehension, pushing result 'into' another Collectable")
+
+_ =
+  for({key, value} <- style, into: %{}, do: {String.to_atom(key), value})
+  |> IO.inspect()
+
+# ---------------------------------------
