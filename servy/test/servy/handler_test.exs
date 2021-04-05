8c064e80e3e34c4727ebd763bf936a07259e526e
rafaelbiten <rafaelbiten@icloud.com>
2021-04-05 14:06:48 -0400

    [SPEC] Adds a few more tests for Servy.Handler

--- servy/test/servy/handler_test.exs
+++ servy/test/servy/handler_test.exs
@@ -8,8 +8,7 @@ defmodule Servy.HandlerTest do
     GET /wildthings HTTP/1.1\r
     Host: example.com\r
     User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
+    Accept: */*
     """
 
     assert Handler.handle(request) == """
@@ -19,5 +18,82 @@ defmodule Servy.HandlerTest do
            \r
            😃 Bears, Leões, Tigers 😃
            """
+  end
+
+  test "GET /wildlife rewrites path to /wildthings" do
+    request = """
+    GET /wildlife HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*
+    """
+
+    assert Handler.handle(request) == """
+           HTTP/1.1 200 OK\r
+           Content-Type: text/html\r
+           Content-Length: 31\r
+           \r
+           😃 Bears, Leões, Tigers 😃
+           """
+  end
+
+  test "GET /bears/1" do
+    request = """
+    GET /bears/1 HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = Handler.handle(request)
+
+    assert remove_whitespaces(response) ==
+             remove_whitespaces("""
+             HTTP/1.1 200 OK\r
+             Content-Type: text/html\r
+             Content-Length: 83\r
+             \r
+             😃<h1>ShowingBear</h1><p>IsTeddyhibernating?<strong>true</strong></p>😃
+             """)
+  end
+
+  test "DELETE /bears/1" do
+    request = """
+    DELETE /bears/1 HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    assert Handler.handle(request) == """
+           HTTP/1.1 403 Forbidden\r
+           Content-Type: text/html\r
+           Content-Length: 31\r
+           \r
+           It's forbidden to delete bears.
+           """
+  end
+
+  test "404s" do
+    request = """
+    GET /unknown HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*
+    """
+
+    assert Handler.handle(request) == """
+           HTTP/1.1 404 Not Found\r
+           Content-Type: text/html\r
+           Content-Length: 45\r
+           \r
+           The resource for /unknown could not be found.
+           """
+  end
+
+  defp remove_whitespaces(text) do
+    String.replace(text, ~r{\s}, "")
   end
 end
