baeb5613514442aa5acf8bfa9c2f93c5656a5f98
rafaelbiten <rafaelbiten@icloud.com>
2021-04-06 13:41:30 -0400

    [FEAT] Servy.Markdown to serve .md files as html

---
+++ servy/lib/servy/markdown.ex
@@ -0,0 +1,10 @@
+defmodule Servy.Markdown do
+  @markdown_path Path.expand("markdown", File.cwd!())
+
+  def faq() do
+    @markdown_path
+    |> Path.join("faq.md")
+    |> File.read!()
+    |> Earmark.as_html!()
+  end
+end
---
+++ servy/test/servy/markdown_test.exs
@@ -0,0 +1,16 @@
+defmodule Servy.MarkdownTest do
+  use ExUnit.Case
+  doctest Servy.Markdown
+  alias Servy.Markdown
+
+  import TestHelper, only: [remove_whitespace: 1]
+
+  describe "faq/0" do
+    test "returns faq.md as html" do
+      faq = Markdown.faq()
+
+      assert remove_whitespace(faq) ==
+               ~s{<h1>FrequentlyAskedQuestions</h1><ul><li><p><strong>HaveyoureallyseenBigfoot?</strong></p><p>Yes!Inthis<ahref=\"https://www.youtube.com/watch?v=v77ijOO8oAk\">totallybelievablevideo</a>!</p></li><li><p><strong>No,ImeanseenBigfoot<em>ontherefuge</em>?</strong></p><p>Oh!Notyet,butwe’restilllooking…</p></li><li><p><strong>Canyoujustshowmesomecode?</strong></p><p>Sure!Here’ssomeElixir:</p><pre><codeclass=\"elixir\">[&quot;Bigfoot&quot;,&quot;Yeti&quot;,&quot;Sasquatch&quot;]|&gt;Enum.random()</code></pre></li></ul>}
+    end
+  end
+end
