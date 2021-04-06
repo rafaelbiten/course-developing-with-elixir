5172eeb4f9992f0cc242294d9a2a2fe4746495f8
rafaelbiten <rafaelbiten@icloud.com>
2021-04-06 11:03:36 -0400

    [REFACT] Returns empty results on file read error

--- servy/lib/servy/db.ex
+++ servy/lib/servy/db.ex
@@ -1,10 +1,19 @@
 defmodule Servy.Db do
   @db_path Path.expand("db", File.cwd!())
-  @table_bears Path.join(@db_path, "bears.json")
+  @bears Path.join(@db_path, "bears.json")
 
   def all(Servy.Bear) do
-    @table_bears
-    |> File.read!()
-    |> Poison.decode!(as: [%Servy.Bear{}])
+    read_file(@bears, &Poison.decode!(&1, as: [%Servy.Bear{}]))
+  end
+
+  defp read_file(table, decode_fn) do
+    case File.read(table) do
+      {:ok, entities} ->
+        decode_fn.(entities)
+
+      {:error, reason} ->
+        IO.inspect("Can't read #{table}: #{reason}")
+        []
+    end
   end
 end
