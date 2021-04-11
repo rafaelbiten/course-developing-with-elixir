8bf17a16474f884ed74dfed3b092281dbce67d0c
rafaelbiten <rafaelbiten@icloud.com>
2021-04-11 18:12:01 -0400

    [CHORE] Added new aliases to mix.exs

--- servy/mix.exs
+++ servy/mix.exs
@@ -7,7 +7,9 @@ defmodule Servy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
@@ -21,12 +23,17 @@ defmodule Servy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:cortex, "~> 0.6.0", only: [:dev, :test]},
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:cortex, "~> 0.6.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:earmark, "~> 1.4"},
       {:poison, "~> 4.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      check: ["dialyzer", "test"],
+      get: ["deps.get", "deps.compile"]
     ]
   end
 end
