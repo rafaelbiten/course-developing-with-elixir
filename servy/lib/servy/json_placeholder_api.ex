4cd21e4d964f5673f9066c0f37e47f2b83eb9f31
rafaelbiten <rafaelbiten@icloud.com>
2021-04-27 22:14:40 -0400

    [REFACT] No need to decode and encode

--- servy/lib/servy/json_placeholder_api.ex
+++ servy/lib/servy/json_placeholder_api.ex
@@ -1,20 +1,20 @@
 defmodule Servy.JsonPlaceholderApi do
   use Tesla
 
-  plug(Tesla.Middleware.JSON, engine: Poison)
+  # plug(Tesla.Middleware.JSON, engine: Poison)
   plug(Tesla.Middleware.BaseUrl, "https://jsonplaceholder.typicode.com")
 
   alias Servy.Conn
 
   def index(%Conn{} = conn) do
     {:ok, response} = get("/users")
-    resp_body = Poison.encode!(response.body)
-    %{conn | status: 200, resp_body: resp_body, resp_content_type: "application/json"}
+    # resp_body = Poison.encode!(response.body)
+    %{conn | status: 200, resp_body: response.body, resp_content_type: "application/json"}
   end
 
   def get_user(%Conn{} = conn, %{"id" => id} = _params) do
     {:ok, response} = get("/users/" <> id)
-    resp_body = Poison.encode!(response.body)
-    %{conn | status: 200, resp_body: resp_body, resp_content_type: "application/json"}
+    # resp_body = Poison.encode!(response.body)
+    %{conn | status: 200, resp_body: response.body, resp_content_type: "application/json"}
   end
 end
