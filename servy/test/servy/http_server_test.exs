7cc29a3076348df0b64db12d679344789ee9dae6
rafaelbiten <rafaelbiten@icloud.com>
2021-05-02 18:29:41 -0400

    [SPEC] Quick check on multiple endpoints

--- servy/test/servy/http_server_test.exs
+++ servy/test/servy/http_server_test.exs
@@ -23,6 +23,25 @@ defmodule Servy.HttpServerTest do
     |> Enum.map(&assert_response/1)
   end
 
+  @tag :capture_log
+  test "multiple endpoints responding with status 200" do
+    [
+      "/snapshots",
+      "/api/bears",
+      "/bears",
+      "/bears/1",
+      "/pages/about",
+      "/about"
+    ]
+    |> Enum.map(fn endpoint ->
+      port = start_server()
+      host = "http://localhost:#{port}"
+      Task.async(Tesla, :get, [host <> endpoint])
+    end)
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(fn {:ok, response} -> assert response.status == 200 end)
+  end
+
   defp start_server do
     port = Enum.random(1024..2000)
     spawn(HttpServer, :start, [port])
