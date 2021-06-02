6ec10859070efb5dc29c073403bbe439042ad245
rafaelbiten <rafaelbiten@icloud.com>
2021-06-01 22:49:57 -0400

    [FEAT] Simulating sending and receiving data

--- servy/lib/servy/pledge_ctrl.ex
+++ servy/lib/servy/pledge_ctrl.ex
@@ -1,8 +1,31 @@
 defmodule Servy.PledgeCtrl do
+  use Tesla
+  plug(Tesla.Middleware.Headers, [{"content-type", "application/json"}])
+  plug(Tesla.Middleware.BaseUrl, "https://httparrot.herokuapp.com")
+
   alias Servy.PledgeAgent
 
   def create(conn, %{"name" => name, "amount" => amount}) do
     PledgeAgent.create(name, String.to_integer(amount))
+
+    {:ok, raw_response} =
+      Task.async(fn ->
+        post(
+          "/post",
+          Poison.encode!(%{
+            event: "Pledge Created",
+            name: name,
+            amount: amount
+          })
+        )
+      end)
+      |> Task.await()
+
+    Poison.decode!(raw_response.body)
+    |> Map.get("data")
+    |> Poison.decode!()
+    |> IO.inspect(label: "Data")
+
     %{conn | status: 201, resp_body: "#{name} pledged #{amount}"}
   end
 
