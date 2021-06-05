51dbbf457052ea654e48a9ca6cddc01af57fe0b9
rafaelbiten <rafaelbiten@icloud.com>
2021-06-04 20:33:12 -0400

    [REFACT] Changing file extentions

--- servy/lib/servy/pledge_view.ex
+++ servy/lib/servy/pledge_view.ex
@@ -3,12 +3,12 @@ defmodule Servy.PledgeView do
 
   @templates_path Path.expand("templates", File.cwd!())
 
-  EEx.function_from_file(:def, :new, Path.join(@templates_path, "pledge_new.eex"), [])
+  EEx.function_from_file(:def, :new, Path.join(@templates_path, "pledge_new.html"), [])
 
   EEx.function_from_file(
     :def,
     :recent_pledges,
-    Path.join(@templates_path, "pledges_recent.eex"),
+    Path.join(@templates_path, "pledges_recent.html.eex"),
     [:pledges]
   )
 end
