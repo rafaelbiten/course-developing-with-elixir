d92dee3ae131ea614c34cfae63abe02ec0e9341a
rafaelbiten <rafaelbiten@icloud.com>
2021-06-21 08:28:16 -0400

    [FEAT] Starting services with ServicesSupervisor

--- servy/.iex.exs
+++ servy/.iex.exs
@@ -8,8 +8,5 @@ alias Playground.Messages
 alias Servy.{ PledgeServer, PledgeCtrl }
 
 if (Application.get_env(:servy, :environment) == :dev) do
-  Servy.Count404s.start_link(%{})
-  Servy.PledgeAgent.start_link([])
-  Servy.SensorServer.start_link()
-  spawn_link(Servy.HttpServer, :start, [4000])
+  Servy.ServicesSupervisor.start_link()
 end
--- servy/lib/servy/services_supervisor.ex
+++ servy/lib/servy/services_supervisor.ex
@@ -1,14 +1,24 @@
 defmodule Servy.ServicesSupervisor do
   use Supervisor
 
+  alias Servy.Count404s
+  alias Servy.PledgeServer
+  alias Servy.SensorServer
+  alias Servy.HttpServerGenServer
+
   def start_link do
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     children = [
-      Servy.PledgeServer,
-      Servy.SensorServer
+      Count404s,
+      PledgeServer,
+      {SensorServer,
+       %SensorServer.State{
+         refresh_interval: :timer.minutes(10)
+       }},
+      HttpServerGenServer
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
